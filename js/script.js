8691e1d3bda35d64e889190ddd94e9386af250f2
danielle Manguele <danielemanguele@gmail.com>
2019-12-17 13:22:57 -0600

    adding animations anad medias queries

--- js/script.js
+++ js/script.js
@@ -33,10 +33,37 @@ $(document).ready(function () {
          $('#navbar-brand-span').toggleClass('hide');
     });
  
-$('.navbar-toggler').on('click', function(){
+ $('.navbar-toggler').on('click', function(){
     $('#navbar-brand-span').toggleClass('hide');
     $('.collapse').addClass('no-transition').collapse('toggle');
     $('.animated-icon').toggleClass('open');
-});
+ });
+ //*************************about section animation starts here*********************////////
+ var controller= new ScrollMagic.Controller();
+    var scene= new ScrollMagic.Scene({
+      triggerElement:'.thecinematics'
+    })
+    .setClassToggle('.thecinematics','show')
+    .addTo(controller)
+
+
+ var controller1= new ScrollMagic.Controller();
+    var scene1= new ScrollMagic.Scene({
+      triggerElement:'.col-12 p'
+    })
+    .setClassToggle('.col-12 p','show')
+    .addTo(controller1)
+
+    var controller2= new ScrollMagic.Controller();
+    var scene2= new ScrollMagic.Scene({
+      triggerElement:'#mybios'
+    })
+    .setClassToggle('#mybios','show')
+    .addTo(controller2)
+
+ //***********************About section animation ends here*************************** */
+
+
+
     //document.ready ends here
 });
